7ac76c21f387db9a47b838bb92bc75bdf66239fd
Kartik <kartikkc397@outlook.com>
2024-06-12 01:04:19 +0530

    Fix: Mongoose Error

--- db.js
+++ db.js
@@ -4,11 +4,11 @@ const mongoose = require("mongoose");
 const mongoURL = process.env.MONGODB_URL;
 
 const connectToMongo = () => {
-    mongoose.connect(mongoURL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
-        // mongoose.connect(mongoURL)
+    // mongoose.connect(mongoURL, {
+    //     useNewUrlParser: true,
+    //     useUnifiedTopology: true,
+    // })
+        mongoose.connect(mongoURL)
         .then(
             console.log("[STATUS] Connected To The Database")
         ).catch(error => console.error(error));
