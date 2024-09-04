127a42e133b4078b1b2d211cb339e92a47cae241
Kartik <63699242+kartikkc@users.noreply.github.com>
2024-09-04 18:37:50 +0530

    Fix: Regenerated OTP mailer
    Now, The regenerated OTPs will be sent to the mail, which the user have used.

--- routes/regenOTP.js
+++ routes/regenOTP.js
@@ -12,8 +12,11 @@ router.post("/", async (req, res) => {
         }
         else {
             const userID = getID._id;
-            const response = await OtpGen(userID);
-            res.json(response);
+            // const response = await OtpGen(userID);
+            // res.json(response);
+             const response = await OtpGen(userID);
+             await mailer(response, getID.name, getID.email);
+             res.status(200).json({Message:"Otp Sent on "+getID.email});
         }
     }
     else {
